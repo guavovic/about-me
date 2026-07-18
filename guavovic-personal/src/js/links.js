454f7c8ea3e4b6fa5cac47a780f997d9d970e476
Gustavo Victor F. Pinheiro <99424489+guavovic@users.noreply.github.com>
2026-07-18 15:24:45 -0300

    feat: update profile links and replace Spotify with Last.fm

--- guavovic-personal/src/js/links.js
+++ guavovic-personal/src/js/links.js
@@ -1,13 +1,13 @@
 const LINKS = {
-    linkedin: "https://www.linkedin.com/in/gustavo-victor/",
+    linkedin: "https://www.linkedin.com/in/gustavo-victor-pinheiro/",
     github: "https://github.com/guavovic",
-    instagram: "https://instagram.com/guavovic",
-    spotify: "https://open.spotify.com/user/55rmruj3u8vmwlo23q9iub5oi?si=f3a553501d5d4b3f",
-    email: "mailto:gustavopinheirocontact@gmail.com"
+    instagram: "https://instagram.com/victorguavo",
+    lastfm: "https://www.last.fm/user/guavovic",
+    email: "mailto:gustavo.victor.pinheiro@gmail.com"
 };
 
 document.getElementById('linkedin-link').href = LINKS.linkedin;
 document.getElementById('github-link').href = LINKS.github;
 document.getElementById('instagram-link').href = LINKS.instagram;
-document.getElementById('spotify-link').href = LINKS.spotify;
+document.getElementById('lastfm-link').href = LINKS.lastfm;
 document.getElementById('email-link').href = LINKS.email;
